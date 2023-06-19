14a256e7fae367edf85851482af38f7930ca2122
smart raysam <raysamtob@gmail.com>
2023-06-19 10:39:09 +0100

    update data model

--- payloadModel.py
+++ payloadModel.py
@@ -4,24 +4,24 @@ import datetime
 
 class PingResult:
     def __init__(self, svr_ip_ip_address, svr_ip_status, ping_date_time):
-        self.svr_ip_ip_address = svr_ip_ip_address
-        self.svr_ip_status = svr_ip_status
-        self.ping_date_time = ping_date_time
+        self.SvrIpIpAddress = svr_ip_ip_address
+        self.SvrIpStatus = svr_ip_status
+        self.PingDateTime = ping_date_time
 
 class ServerData:
     def __init__(self, hostname, ram_total, drive_space, cpu, ram_free):
-        self.hostname = hostname
-        self.ram_total = ram_total
-        self.drive_space = drive_space
-        self.cpu = cpu
-        self.ram_free = ram_free
+        self.Hostname = hostname
+        self.RamTotal = ram_total
+        self.DriveSpace = drive_space
+        self.Cpu = cpu
+        self.RamFree = ram_free
 
 class DataToPost:
     def __init__(self, license_key, org_id, server_data, ping_result):
-        self.license_key = license_key
-        self.org_id = org_id
-        self.server_data = server_data
-        self.ping_result = ping_result
+        self.licenceKey = license_key
+        self.OrgID = org_id
+        self.Serverdata = server_data
+        self.Pingresult = ping_result
 
 
 class ServerDataEncoder(json.JSONEncoder):
