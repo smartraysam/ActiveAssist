e1c457a99edd1da773e8c7762346b96a489fdd27
smart raysam <raysamtob@gmail.com>
2023-06-20 18:31:08 +0100

    Update startup script

--- startup_run.bat
+++ startup_run.bat
@@ -1,3 +1,7 @@
 @echo off
-start /min python C:\ActiveAssist\index.py
-exit
+
+REM Activate the virtual environment
+call  C:\ActiveAssist\venv\Scripts\activate.bat
+
+REM Start the Python script with minimized command prompt
+start /min cmd /c "python C:\ActiveAssist\index.py"
