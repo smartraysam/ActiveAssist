a21489ff40d3c0da381ec57e4076d3746c2a40b1
smart raysam <raysamtob@gmail.com>
2023-07-10 12:41:57 +0100

    update

--- utils/ping.py
+++ utils/ping.py
@@ -1,5 +1,7 @@
 import datetime
 import socket
+
+import ray
 from data.datamodel import PingResult
 import ping3
 import aioping
@@ -15,10 +17,9 @@ async def AsyncPing(ip_address):
         return PingResult(ip_address, "Fail", datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
         socket.close()
 
-
 def ping_ip_address(ip_address):
     try:
-        response_time = ping3.ping(ip_address)
+        response_time = ping3.ping(ip_address, timeout=2)
         if response_time is not None:
             return PingResult(ip_address, "Success", datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
         else:
